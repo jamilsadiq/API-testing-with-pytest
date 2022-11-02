458c5716fdf4e39b1f03c40f800ad2db0082996a
Md Sadiquzzaman <jamilsadiq00@gmail.com>
2022-11-02 13:43:53 +0600

    Update test_delete_api.py

--- test_delete_api.py
+++ test_delete_api.py
@@ -3,7 +3,7 @@ import pytest
 import requests
 import json
 
-def test_post_data(supply_url):
+def test_delete_data(supply_url):
 	url = supply_url + "/users/2" 
 	resp = requests.delete(url)
 	assert resp.status_code == 204
